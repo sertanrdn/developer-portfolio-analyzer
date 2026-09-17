ddd7709773bce61605f22c01885b556d187bad0d
sertanrdn <sertanerdogan07@gmail.com>
2026-09-17 18:22:03 +0200

    Fetched public repositories from github API

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import requests
+
 username = input("Enter GitHub username: ")
 cleaned_username = username.strip()
 
@@ -5,3 +7,12 @@ if not cleaned_username:
     print("Error: GitHub username is required.")
 else:
     print("GitHub username:", cleaned_username)
+
+    url = f"https://api.github.com/users/{cleaned_username}/repos"
+    print(url)
+
+    response = requests.get(url)
+    print(response.status_code)
+
+    repositories = response.json()
+    print(type(repositories))
