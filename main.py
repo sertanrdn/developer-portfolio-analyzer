2959249b793ab0802a58488066432082cb7cf7a5
sertanrdn <sertanerdogan07@gmail.com>
2026-09-20 22:27:10 +0200

    Moved repository analysis into separate module

--- main.py
+++ main.py
@@ -2,7 +2,13 @@ import os
 
 from dotenv import load_dotenv
 import requests
-from datetime import datetime, timezone
+from analysis import (
+    process_repositories,
+    get_repository_summary,
+    analyze_languages,
+    analyze_metadata_coverage,
+    analyze_recent_activity
+) 
 
 load_dotenv()
 github_token = os.getenv("GITHUB_TOKEN")
@@ -14,142 +20,6 @@ else:
 
 username = input("Enter GitHub username: ")
 cleaned_username = username.strip()
-
-def process_repositories(repositories):
-    processed_repositories = []
-
-    for repository in repositories:
-        repo_data = {
-            "name": repository.get("name"),
-            "description": repository.get("description"),
-            "language": repository.get("language"),
-            "topics": repository.get("topics", []),
-            "fork": repository.get("fork"),
-            "archived": repository.get("archived"),
-            "created_at": repository.get("created_at"),
-            "pushed_at": repository.get("pushed_at")
-        }
-
-        processed_repositories.append(repo_data)
-    return processed_repositories
-
-def get_repository_summary(processed_repositories):
-    # Get the repo summary count
-    total_repositories = len(processed_repositories)
-    forked_repositories = 0
-    archived_repositories = 0
-
-    for repository in processed_repositories:
-        if repository.get("fork"):
-            forked_repositories += 1
-        
-        if repository.get("archived"):
-            archived_repositories += 1
-    
-    original_repositories = total_repositories - forked_repositories
-
-    summary_repositories = {
-        "total": total_repositories,
-        "original": original_repositories,
-        "forked": forked_repositories,
-        "archived": archived_repositories
-    }
-
-    return summary_repositories
-
-def analyze_languages(processed_repositories):
-    # Language analysis for repos
-    language_counts = {}
-    no_language_count = 0
-
-    for repository in processed_repositories:
-        if not repository.get("fork"):
-            language = repository.get("language")
-
-            if language is None:
-                no_language_count += 1
-            else:
-                if language in language_counts:
-                    language_counts[language] += 1
-                else:
-                    language_counts[language] = 1
-
-    return language_counts, no_language_count
-
-def analyze_metadata_coverage(processed_repositories, original_repositories):
-    # Get the description and topic coverages for repos
-    repo_with_desc = 0
-    repo_without_desc = 0
-
-    for repository in processed_repositories:
-        if not repository.get("fork"):
-            if repository.get("description"):
-                repo_with_desc += 1
-            else:
-                repo_without_desc += 1
-
-    repo_with_topics = 0
-    repo_without_topics = 0
-
-    for repository in processed_repositories:
-        if not repository.get("fork"):
-            if repository.get("topics"):
-                repo_with_topics += 1
-            else: 
-                repo_without_topics += 1
-
-    if original_repositories > 0:
-        description_coverage = round((repo_with_desc / original_repositories) * 100, 2)
-        topics_coverage = round((repo_with_topics / original_repositories) * 100, 2)
-    else:
-        description_coverage = 0
-        topics_coverage = 0
-
-    metadata_analysis = {
-        "with_description": repo_with_desc,
-        "without_description": repo_without_desc,
-        "description_coverage": description_coverage,
-        "with_topics": repo_with_topics,
-        "without_topics": repo_without_topics,
-        "topics_coverage": topics_coverage
-    }
-
-    return metadata_analysis
-
-def analyze_recent_activity(processed_repositories):
-    # Calculating the recent activity
-    recent_repositories = []
-
-    for repository in processed_repositories:
-        if not repository.get("fork"):
-            date_string = repository.get("pushed_at")
-
-            if date_string:
-                date_object = datetime.fromisoformat(date_string)
-
-                activity_data = {
-                    "name": repository.get("name"),
-                    "pushed_at": date_object
-                }
-                recent_repositories.append(activity_data)
-
-    sorted_repositories = sorted(
-        recent_repositories,
-        key=lambda repository: repository["pushed_at"],
-        reverse=True
-    )
-
-    # Get the activity within last 90 days
-    recently_active_count = 0
-    current_time = datetime.now(timezone.utc)
-
-    for repository in sorted_repositories:
-        time_since_push = current_time - repository["pushed_at"]
-
-        if time_since_push.days <= 90:
-            recently_active_count += 1
-
-    return sorted_repositories, recently_active_count
 
 if not cleaned_username:
     print("Error: GitHub username is required.")
